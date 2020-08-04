03de960c4b93ed5ab285c7ee5781e5a0aef0f1d7
Caleb Owens <caleb.ja.owens@gmail.com>
2020-08-03 23:32:53 -0400

    Abandoning this bot for now. Making a second one.

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ async def on_member_remove(member):
     print(f'{member} has left the server!')
 
 # Commands
-
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
@@ -32,3 +31,19 @@ async def _8ball(ctx, *, question):
         'Yes Senpai',
     ]
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+# Clear Command
+    # = 5 is default value
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send(f'{amount} messages have been cleared!')
+
+# Kick/Ban
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
